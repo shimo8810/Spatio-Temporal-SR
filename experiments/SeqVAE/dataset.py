12449fee23321a7c4cabf42232258c8772beecbc
Hayato Shimodaira <shimodaira@cmu.iit.tsukuba.ac.jp>
2018-10-03 12:06:03 +0900

    update: sequenceデータセットを追加

--- experiments/SeqVAE/dataset.py
+++ experiments/SeqVAE/dataset.py
@@ -29,8 +29,34 @@ class COILDataset(dataset_mixin.DatasetMixin):
     def get_example(self, i):
         return self.data[i]
 
+class SeqCOILDataset(dataset_mixin.DatasetMixin):
+    def __init__(self):
+        seq_coil_path = DATA_PATH.joinpath('seq-coil-100')
+        data = []
+        for obj_path in tqdm(seq_coil_path.iterdir()):
+            obj_name = obj_path.name
+            seqs = []
+            for ang in range(0, 360, 5):
+                image_path = obj_path.joinpath('{}__{}.png'.format(obj_name, ang))
+                image = np.asarray(Image.open(image_path)).astype('f').transpose(2, 0, 1) / 255.0
+                seqs.append(image)
+            data.append(seqs)
+        self.data = np.array(data)
+        self.num_obj, self.num_frame, _, _, _ = self.data.shape
+
+    def __len__(self):
+        return self.num_obj * self.num_frame
+
+    def get_example(self, i):
+        obj = i // self.num_frame
+        frame = i % self.num_frame
+        frames = [frame, (frame + 1) % self.num_frame, (frame + 2) % self.num_frame]
+
+        return self.data[obj, frames,:]
+
 if __name__ == '__main__':
-    dataset = COILDataset()
+    dataset = SeqCOILDataset()
     print(len(dataset))
-    img = dataset.get_example(0)
-    print(img.shape, img.dtype)
+    print(dataset.get_example(71).shape)
+    print(dataset.get_example(72).shape)
+    print(dataset.get_example(73).shape)
