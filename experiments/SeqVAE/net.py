fb8c3aa87f09d8e6d9a54b8622a132249957fc0a
Shimo <shimodaira@cmu.iit.tsukuba.ac.jp>
2018-11-08 16:40:31 +0900

    update: 無駄なresモデルを削除, チャネル数に対してスケール性をもたせた

--- experiments/SeqVAE/net.py
+++ experiments/SeqVAE/net.py
@@ -9,7 +9,7 @@ class ConvBNR(chainer.Chain):
     """
     Convolution -> (BatchNormalization) -> (Dropout) -> Activation
     """
-    def __init__(self, ch_in, ch_out, sample="down", use_bn=True, activation=F.relu, use_dp=False):
+    def __init__(self, ch_in, ch_out, sample="down", use_bn=True, activation=F.leaky_relu, use_dp=False):
         self.use_bn = use_bn
         self.activation = activation
         self.use_dp = use_dp
@@ -40,34 +40,15 @@ class ConvBNR(chainer.Chain):
             h = self.activation(h)
         return h
 
-class ResBlock(chainer.Chain):
-    def __init__(self, in_ch, mid_ch, activation=F.relu):
-        self.activation = activation
-        init_w = chainer.initializers.HeNormal()
-        super(ResBlock, self).__init__()
-
-        with self.init_scope():
-            self.conv1 = L.Convolution2D(in_ch, mid_ch, 1, 1, 0, initialW=init_w)
-            self.bn1 = L.BatchNormalization(mid_ch)
-            self.conv2 = L.Convolution2D(mid_ch, mid_ch, 3, 1, 1, initialW=init_w)
-            self.bn2 = L.BatchNormalization(mid_ch)
-            self.conv3 = L.Convolution2D(mid_ch, in_ch, 1, 1, 0, initialW=init_w)
-            self.bn3 = L.BatchNormalization(mid_ch)
-
-    def forward(self, x):
-        h = self.activation(self.bn1(self.conv1(x)))
-        h = self.activation(self.bn2(self.conv2(h)))
-        return self.activation(self.bn3(self.conv3(h)) + x)
-
 class SeqVAE(chainer.Chain):
-    def __init__(self, size, n_latent, n_ch, activation=F.relu):
-        self.n_ch = n_ch
-        self.n_latent = n_latent
-        self.size = size
-        # self.z_size = (n_ch * (2^4)) * (size // (2^4)) ^ 2
-        self.z_ch = n_ch * (2**4)
-        self.z_size = size // (2**4)
-        self.z_dim = self.z_ch * (self.z_size**2)
+    def __init__(self, size, ch, n_latent, ch_scale, activation=F.relu):
+        self.ch = ch # データのチャネル数
+        self.size = size # データの縦横サイズ(同じ以外のデータを食わせる予定はない)
+        self.ch_scale = ch_scale # チャネル数のスケール
+        self.n_latent = n_latent # 潜在変数次元
+        self.z_ch = ch_scale * (2**4) # 潜在変数に変換する直前のMAPのチャネル数
+        self.z_size = size // (2**4) # 潜在変数に変換する直前のMAPのサイズ
+        self.n_flat = self.z_ch * (self.z_size**2) # 潜在変数に変換する前のMAPをフラットにしたサイズ
 
         init_w = chainer.initializers.HeNormal()
 
@@ -75,22 +56,21 @@ class SeqVAE(chainer.Chain):
 
         with self.init_scope():
             # encoder 128 -> 128 -> 64 -> 32 -> 16 -> 8
-            self.enc_conv0 = L.Convolution2D(None, n_ch, 3, 1, 1, initialW=init_w)
-            self.enc_conv1 = ConvBNR(n_ch * 1, n_ch * 2, activation=activation)
-            self.enc_conv2 = ConvBNR(n_ch * 2, n_ch * 4, activation=activation)
-            self.enc_conv3 = ConvBNR(n_ch * 4, n_ch * 8, activation=activation)
-            self.enc_conv4 = ConvBNR(n_ch * 8, n_ch * 16, activation=activation)
-            self.enc_fc5 = L.Linear(self.z_dim, 2000)
-            self.enc_fc6_mu = L.Linear(2000, n_latent)
-            self.enc_fc6_ln_var = L.Linear(2000, n_latent)
+            self.enc_conv0 = L.Convolution2D(None, ch_scale, 3, 1, 1, initialW=init_w)
+            self.enc_conv1 = ConvBNR(ch_scale * 1, ch_scale *  2, activation=activation)
+            self.enc_conv2 = ConvBNR(ch_scale * 2, ch_scale *  4, activation=activation)
+            self.enc_conv3 = ConvBNR(ch_scale * 4, ch_scale *  8, activation=activation)
+            self.enc_conv4 = ConvBNR(ch_scale * 8, ch_scale * 16, activation=activation)
+            self.enc_fc5_mu = L.Linear(self.n_flat, n_latent)
+            self.enc_fc5_ln_var = L.Linear(self.n_flat, n_latent)
 
             # decoder
-            self.dec_fc0 = L.Linear(n_latent, self.z_dim)
-            self.dec_conv1 = ConvBNR(n_ch *16, n_ch * 8, sample="up")
-            self.dec_conv2 = ConvBNR(n_ch * 8, n_ch * 4, sample="up")
-            self.dec_conv3 = ConvBNR(n_ch * 4, n_ch * 2, sample="up")
-            self.dec_conv4 = ConvBNR(n_ch * 2, n_ch * 1, sample="up")
-            self.dec_conv5 = L.Convolution2D(n_ch, 3, 3, 1, 1, initialW=init_w)
+            self.dec_fc0 = L.Linear(n_latent, self.n_flat)
+            self.dec_conv1 = ConvBNR(ch_scale * 16, ch_scale * 8, sample="up")
+            self.dec_conv2 = ConvBNR(ch_scale *  8, ch_scale * 4, sample="up")
+            self.dec_conv3 = ConvBNR(ch_scale *  4, ch_scale * 2, sample="up")
+            self.dec_conv4 = ConvBNR(ch_scale *  2, ch_scale * 1, sample="up")
+            self.dec_conv5 = L.Convolution2D(ch_scale, self.ch, 3, 1, 1, initialW=init_w)
 
     def encode(self, x):
         h = F.leaky_relu(self.enc_conv0(x))
@@ -98,9 +78,8 @@ class SeqVAE(chainer.Chain):
         h = self.enc_conv2(h)
         h = self.enc_conv3(h)
         h = self.enc_conv4(h)
-        h = F.tanh(self.enc_fc5(h))
-        mu = self.enc_fc6_mu(h)
-        ln_var = self.enc_fc6_ln_var(h)
+        mu = self.enc_fc5_mu(h)
+        ln_var = self.enc_fc5_ln_var(h)
         return mu, ln_var
 
     def decode(self, z, sig=True):
@@ -111,125 +90,6 @@ class SeqVAE(chainer.Chain):
         h = self.dec_conv3(h)
         h = self.dec_conv4(h)
         h = self.dec_conv5(h)
-        if sig:
-            return F.sigmoid(h)
-        else:
-            return h
-
-    def forward(self, x, sig=True):
-        return self.decode(self.encode(x)[0], sig)
-
-    def get_loss_func(self, C=1.0, k=1):
-        def lf(x):
-            mu, ln_var = self.encode(x)
-            batch_size = len(mu.data)
-
-            # reconstruction loss
-            rec_loss = 0
-            for l in range(k):
-                z = F.gaussian(mu, ln_var)
-                rec_loss += F.bernoulli_nll(x, self.decode(z, sig=False)) / (k * batch_size)
-            self.rec_loss = rec_loss
-
-            # kl-divergnece regularization
-            self.kl_loss = F.gaussian_kl_divergence(mu, ln_var) / batch_size
-            self.loss = self.rec_loss + C * self.kl_loss
-            chainer.report({'rec_loss': self.rec_loss,
-                'kl_loss':self.kl_loss, 'loss': self.loss}, observer=self)
-            return self.loss
-        return lf
-
-    def get_seq_loss_func(self, C1=1.0, C2=2.0, k=1):
-        def lf(x):
-            bachsize, numframe, _, _, _ = x.shape
-            x1 = x[:,0,:]
-            x2 = x[:,2,:]
-            mu1, ln_var1 = self.encode(x1)
-            mu2, ln_var2 = self.encode(x2)
-
-            # reconstruction loss
-            rec_loss = 0
-            seq_loss = 0
-            for l in range(k):
-                z1 = F.gaussian(mu1, ln_var1)
-                rec_loss += F.bernoulli_nll(x1, self.decode(z1, sig=False)) / (2 * k * bachsize)
-                z2 = F.gaussian(mu2, ln_var2)
-                rec_loss += F.bernoulli_nll(x2, self.decode(z1, sig=False)) / (2 * k * bachsize)
-
-                zc = (z1 + z2) / 2
-                seq_loss += F.bernoulli_nll(x[:,1,:], self.decode(zc, sig=False)) / (2 * k * bachsize)
-            self.rec_loss = rec_loss
-            self.seq_loss = seq_loss
-
-            # kl-divergnece regularization
-            self.kl_loss = F.gaussian_kl_divergence(mu1, ln_var1) / bachsize / 2.0
-            self.kl_loss += F.gaussian_kl_divergence(mu2, ln_var2) / bachsize / 2.0
-
-            # loss summation
-            self.loss = self.rec_loss + C1 * self.kl_loss + C2 * self.seq_loss
-            chainer.report({'rec_loss': self.rec_loss,
-                'kl_loss':self.kl_loss, 'seq_loss': self.seq_loss, 'loss': self.loss}, observer=self)
-            return self.loss
-        return lf
-
-class SeqResVAE(chainer.Chain):
-    def __init__(self, size, n_latent, n_ch, activation=F.relu):
-        self.n_ch = n_ch
-        self.n_latent = n_latent
-        self.size = size
-        # self.z_size = (n_ch * (2^4)) * (size // (2^4)) ^ 2
-        self.z_ch = n_ch * (2**4)
-        self.z_size = size // (2**4)
-        self.z_dim = self.z_ch * (self.z_size**2)
-
-        init_w = chainer.initializers.HeNormal()
-
-        super(SeqResVAE, self).__init__()
-
-        with self.init_scope():
-            # encoder 128 -> 128 -> 64 -> 32 -> 16 -> 8
-            self.enc_conv0 = L.Convolution2D(None, n_ch, 3, 1, 1, initialW=init_w)
-            self.enc_conv1 = ConvBNR(n_ch * 1, n_ch * 2, activation=activation)
-            self.enc_conv2 = ConvBNR(n_ch * 2, n_ch * 4, activation=activation)
-            self.enc_conv3 = ConvBNR(n_ch * 4, n_ch * 8, activation=activation)
-            self.enc_conv4 = ConvBNR(n_ch * 8, n_ch * 16, activation=activation)
-            self.enc_fc5 = L.Linear(self.z_dim, 2000)
-            self.enc_fc6_mu = L.Linear(2000, n_latent)
-            self.enc_fc6_ln_var = L.Linear(2000, n_latent)
-
-            # decoder
-            self.dec_fc0 = L.Linear(n_latent, self.z_dim)
-            self.dec_conv1 = ConvBNR(n_ch *16, n_ch * 8, sample="up")
-            self.dec_conv2 = ConvBNR(n_ch * 8, n_ch * 4, sample="up")
-            self.dec_conv3 = ConvBNR(n_ch * 4, n_ch * 2, sample="up")
-            self.dec_conv4 = ConvBNR(n_ch * 2, n_ch * 1, sample="up")
-            self.dec_res5 = ResBlock(n_ch, n_ch)
-            self.dec_res6 = ResBlock(n_ch, n_ch)
-            self.dec_res7 = ResBlock(n_ch, n_ch)
-            self.dec_conv8 = L.Convolution2D(n_ch, 3, 1, 1, 0, initialW=init_w)
-
-    def encode(self, x):
-        h = F.leaky_relu(self.enc_conv0(x))
-        h = self.enc_conv1(h)
-        h = self.enc_conv2(h)
-        h = self.enc_conv3(h)
-        h = self.enc_conv4(h)
-        h = F.tanh(self.enc_fc5(h))
-        mu = self.enc_fc6_mu(h)
-        ln_var = self.enc_fc6_ln_var(h)
-        return mu, ln_var
-
-    def decode(self, z, sig=True):
-        h = F.tanh(self.dec_fc0(z))
-        h = F.reshape(h, (z.shape[0], self.z_ch, self.z_size, self.z_size))
-        h = self.dec_conv1(h)
-        h = self.dec_conv2(h)
-        h = self.dec_conv3(h)
-        h = self.dec_conv4(h)
-        h = self.dec_res5(h)
-        h = self.dec_res6(h)
-        h = self.dec_res7(h)
-        h = self.dec_conv8(h)
         if sig:
             return F.sigmoid(h)
         else:
